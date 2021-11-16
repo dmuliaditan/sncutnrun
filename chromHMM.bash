b71c5df9912c604876d38ba52126a2f86bee0d42
Daniel Muliaditan <48524642+dmuliaditan@users.noreply.github.com>
2021-11-16 17:30:30 +0800

    Update chromHMM.bash

--- chromHMM.bash
+++ chromHMM.bash
@@ -15,3 +15,85 @@ bedtools closest -a /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E3_E4_pr
 #Process to final bed
 awk '{ a[$8]++ } END { for (b in a) { print b } }' /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E3_E4_regions_nearest_gene.bed > \
 /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E3_E4_nearest_genes.bed
+
+#Intersect E1-E3 regions
+#Intersect regions that are in promoters
+bedtools intersect -a /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E3_regions.bed \
+-b /mnt/e/genome_references/hg38_reference/promoter_regions_hg38_sorted.bed -sorted -g /mnt/e/genome_references/hg38_reference/resources_broad_hg38_v0_Homo_sapiens_assembly38.fasta.fai >\
+/mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E3_promoters.bed
+
+#Closest genes in promoters
+bedtools closest -a /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E3_promoters.bed \
+-b /mnt/e/genome_references/hg38_reference/gencode.v32.basic.annotation.bed -g /mnt/e/genome_references/hg38_reference/resources_broad_hg38_v0_Homo_sapiens_assembly38.fasta.fai -d \
+> /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E3_regions_nearest_gene.bed
+
+#Process to final bed
+awk '{ a[$8]++ } END { for (b in a) { print b } }' /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E3_regions_nearest_gene.bed > \
+/mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E3_nearest_genes.bed
+
+
+#Intersect E1_E4 regions
+#Intersect regions that are in promoters
+bedtools intersect -a /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E4_regions.bed \
+-b /mnt/e/genome_references/hg38_reference/promoter_regions_hg38_sorted.bed -sorted -g /mnt/e/genome_references/hg38_reference/resources_broad_hg38_v0_Homo_sapiens_assembly38.fasta.fai >\
+/mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E4_promoters.bed
+
+#Closest genes in promoters
+bedtools closest -a /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E4_promoters.bed \
+-b /mnt/e/genome_references/hg38_reference/gencode.v32.basic.annotation.bed -g /mnt/e/genome_references/hg38_reference/resources_broad_hg38_v0_Homo_sapiens_assembly38.fasta.fai -d \
+> /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E4_regions_nearest_gene.bed
+
+#Process to final bed
+awk '{ a[$8]++ } END { for (b in a) { print b } }' /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E4_regions_nearest_gene.bed > \
+/mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E4_nearest_genes.bed
+
+
+#Intersect E1-E5 regions
+#Intersect regions that are in promoters
+bedtools intersect -a /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E5_regions.bed \
+-b /mnt/e/genome_references/hg38_reference/promoter_regions_hg38_sorted.bed -sorted -g /mnt/e/genome_references/hg38_reference/resources_broad_hg38_v0_Homo_sapiens_assembly38.fasta.fai >\
+/mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E5_promoters.bed
+
+#Closest genes in promoters
+bedtools closest -a /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E5_promoters.bed \
+-b /mnt/e/genome_references/hg38_reference/gencode.v32.basic.annotation.bed -g /mnt/e/genome_references/hg38_reference/resources_broad_hg38_v0_Homo_sapiens_assembly38.fasta.fai -d \
+> /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E5_regions_nearest_gene.bed
+
+#Process to final bed
+awk '{ a[$8]++ } END { for (b in a) { print b } }' /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E5_regions_nearest_gene.bed > \
+/mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E1_E5_nearest_genes.bed
+
+#Intersect E4-E5 regions
+#Intersect regions that are in promoters
+bedtools intersect -a /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E4_E5_regions.bed \
+-b /mnt/e/genome_references/hg38_reference/promoter_regions_hg38_sorted.bed -sorted -g /mnt/e/genome_references/hg38_reference/resources_broad_hg38_v0_Homo_sapiens_assembly38.fasta.fai >\
+/mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E4_E5_promoters.bed
+
+#Closest genes in promoters
+bedtools closest -a /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E4_E5_promoters.bed \
+-b /mnt/e/genome_references/hg38_reference/gencode.v32.basic.annotation.bed -g /mnt/e/genome_references/hg38_reference/resources_broad_hg38_v0_Homo_sapiens_assembly38.fasta.fai -d \
+> /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E4_E5_regions_nearest_gene.bed
+
+#Process to final bed
+awk '{ a[$8]++ } END { for (b in a) { print b } }' /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E4_E5_regions_nearest_gene.bed > \
+/mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E4_E5_nearest_genes.bed
+
+
+#Intersect E5-E4 regions
+#Intersect regions that are in promoters
+bedtools intersect -a /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E5_E4_regions.bed \
+-b /mnt/e/genome_references/hg38_reference/promoter_regions_hg38_sorted.bed -sorted -g /mnt/e/genome_references/hg38_reference/resources_broad_hg38_v0_Homo_sapiens_assembly38.fasta.fai >\
+/mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E5_E4_promoters.bed
+
+#Closest genes in promoters
+bedtools closest -a /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E5_E4_promoters.bed \
+-b /mnt/e/genome_references/hg38_reference/gencode.v32.basic.annotation.bed -g /mnt/e/genome_references/hg38_reference/resources_broad_hg38_v0_Homo_sapiens_assembly38.fasta.fai -d \
+> /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E5_E4_regions_nearest_gene.bed
+
+#Process to final bed
+awk '{ a[$8]++ } END { for (b in a) { print b } }' /mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E5_E4_regions_nearest_gene.bed > \
+/mnt/e/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_E5_E4_nearest_genes.bed
+
+
+
+
