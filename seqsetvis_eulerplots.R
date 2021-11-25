710aa523319467037dae6c9f2338bf271d2b5aed
Daniel Muliaditan <48524642+dmuliaditan@users.noreply.github.com>
2021-11-25 17:35:57 +0800

    Update seqsetvis_eulerplots.R

--- seqsetvis_eulerplots.R
+++ seqsetvis_eulerplots.R
@@ -62,6 +62,8 @@ for (a in seq_along(sample_list)) {
 names(bedlist) <- sub(pattern = "_", replacement = " ", x = sample_list, fixed = T)
 
 olaps <- ssvOverlapIntervalSets(bedlist, maxgap = 10)
+
+#Plot number of Signac peaks
 ssvFeatureBars(olaps) +
   ylab("") +
   scale_x_discrete(labels = names(bedlist)) +
@@ -72,3 +74,22 @@ ssvFeatureBars(olaps) +
         legend.text = element_text(size = 18),
         axis.text.y = element_text(size = 18),
         title = element_text(size = 24))
+
+ssvFeatureBinaryHeatmap(olaps)
+ssvFeatureVenn(object = olaps[,c(1,3)], line_width = 1, line_alpha = 0.5) +
+  theme(legend.text = element_text(size=14))
+ssvFeatureUpset(olaps)
+
+#Venn diagrams with venneuler
+euler <- cbind(
+  "HN120PRI K4me3" = olaps$`HN120PRI K4me3`,
+  "HN120MET K4me3"= olaps$`HN120MET K4me3`
+  )
+
+plot(euler(euler), 
+     quantities = list(cex = 4),
+     labels = F,
+     fill = c("#EA15D3", "#D3EA15"),
+     edges =  F,
+     alpha = 1,
+     ) # "#D3EA15" for Met, "#15D3EA" for PCR
