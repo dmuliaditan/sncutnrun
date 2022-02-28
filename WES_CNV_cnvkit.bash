6e7b17a08127ffb18a01c6028e8b5cc7c43cf8d4
Daniel Muliaditan <48524642+dmuliaditan@users.noreply.github.com>
2022-02-28 18:13:21 +0800

    Update WES_CNV_cnvkit.bash

--- WES_CNV_cnvkit.bash
+++ WES_CNV_cnvkit.bash
@@ -8,14 +8,28 @@ cnvkit.py batch -m hybrid --drop-low-coverage \
 -g /home/daniel/genome_references/hg38_reference/hg38_accessible_regions_5kb.bed \
 --output-reference /home/daniel/snCUT_RUN/wes/CNV/cnvkit/hg38_WES_reference.cnn -d /home/daniel/snCUT_RUN/wes/CNV/cnvkit
 
-for j in HN120PRI_subsampled_sorted HN120MET_subsampled_sorted HN120PCR_subsampled_sorted HN137PRI_subsampled_sorted HN137MET_subsampled_sorted HN137PCR_subsampled_sorted
+for j in HN120PRI_subsampled_sorted HN120MET_subsampled_sorted HN120PCR_subsampled_sorted HN137PRI_subsampled_sorted HN137MET HN137PCR_subsampled_sorted
 do
 
 #CNVkit run on Docker
 #Create reference
 
-cnvkit.py batch --drop-low-coverage -p 8 \
--r /home/daniel/snCUT_RUN/wes/CNV/cnvkit/hg38_WES_reference.cnn \
--d /home/daniel/snCUT_RUN/wes/CNV/cnvkit '/home/daniel/snCUT_RUN/wes/results_dir/final_bams/'"$j"'.bam' --scatter --diagram
+#cnvkit.py batch --drop-low-coverage -p 8 \
+#-r /home/daniel/snCUT_RUN/wes/CNV/cnvkit/hg38_WES_reference.cnn \
+#-d /home/daniel/snCUT_RUN/wes/CNV/cnvkit '/home/daniel/snCUT_RUN/wes/results_dir/final_bams/'"$j"'.bam' --scatter --diagram
+
+#Make 50kb windows
+awk '{ print $1, $2, $3, $6}' '/mnt/d/snCUT_RUN/wes/CNV/cnvkit/'"$j"'.call.cns' > '/mnt/d/snCUT_RUN/wes/CNV/cnvkit/'"$j"'_CN.bed'
+tail -n+2 '/mnt/d/snCUT_RUN/wes/CNV/cnvkit/'"$j"'_CN.bed' > '/mnt/d/snCUT_RUN/wes/CNV/cnvkit/'"$j"'_CN2.bed'
+perl -p -i -e 's/ /\t/g' '/mnt/d/snCUT_RUN/wes/CNV/cnvkit/'"$j"'_CN2.bed'
+bedtools makewindows -b '/mnt/d/snCUT_RUN/wes/CNV/cnvkit/'"$j"'_CN2.bed' -i src -w 50000 > '/mnt/d/snCUT_RUN/wes/CNV/cnvkit/'"$j"'_CN_50kb_binned.bed'
 
 done 
+
+#Intersect all the interval files
+bedtools multiinter -i HN120PRI_subsampled_sorted_CN_50kb_binned.bed HN120MET_subsampled_sorted_CN_50kb_binned.bed HN120PCR_subsampled_sorted_CN_50kb_binned.bed \
+HN137PRI_subsampled_sorted_CN_50kb_binned.bed HN137MET_CN_50kb_binned.bed HN137PCR_subsampled_sorted_CN_50kb_binned.bed \
+-names HN120PRI HN120MET HN120PCR HN137PRI HN137MET HN137PCR > HN120_HN137_CNVKIT_merged.bed
+
+#In R
+
