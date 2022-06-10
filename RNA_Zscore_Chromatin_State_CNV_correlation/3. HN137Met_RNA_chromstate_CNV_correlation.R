9de1091ee65795825d50d6f6392a22781c23da23
Daniel Muliaditan <48524642+dmuliaditan@users.noreply.github.com>
2022-06-10 15:48:38 +0800

    Update and rename HN137Met_RNA_ChromState_CNV_correlation.R to 3. HN137Met_RNA_chromstate_CNV_correlation.R

--- RNA_Zscore_Chromatin_State_CNV_correlation/3. HN137Met_RNA_chromstate_CNV_correlation.R
+++ RNA_Zscore_Chromatin_State_CNV_correlation/3. HN137Met_RNA_chromstate_CNV_correlation.R
@@ -1,4 +1,10 @@
-#Static Chromatin State-CNV-RNAseq-Zscore correlation in the HN137Met cell line
+#3. Static Chromatin State-CNV-RNAseq-Zscore correlation in the HN137Met cell line
+
+#Version: 10/06/2022
+#Author: Daniel Muliaditan
+
+#After correlating gene-expression/CNV and gene-expression/chromatin state separately, 
+#we now correlate gene-expression, CNV and chromatin state together
 
 #Load existing dataset and required packages
 setwd("D:/snCUT_RUN/scripts")
@@ -6,7 +12,7 @@ load(file = "25052022_RNAseq_Zscore_chromatin_state_static_correlation.RData")
 
 library(ggplot2)
 
-#Gene CN, chromatin state, RNA Z-score correlation
+#From the genes with both RNAseq Z-score and chromatin state, find the CN of those genes
 rna_epi_cnv <- rna_epi
 rna_epi_cnv$gene_CN = "NA"
 for (j in seq_along(rna_epi_cnv$GENE)) {
@@ -16,14 +22,19 @@ for (j in seq_along(rna_epi_cnv$GENE)) {
 }
 
 dim(rna_epi_cnv)
-rna_epi_cnv <- rna_epi_cnv[complete.cases(rna_epi_cnv),]
+rna_epi_cnv <- rna_epi_cnv[complete.cases(rna_epi_cnv),] #Remove genes with NA CN
 dim(rna_epi_cnv)
+
+#Remove genes with 0 CN
 zeroCNind <- which(rna_epi_cnv$gene_CN == 0)
 rna_epi_cnv <- rna_epi_cnv[-zeroCNind,]
 rna_epi_cnv$gene_CN <- as.numeric(rna_epi_cnv$gene_CN)
+
+#Recategorize genes with higher CN as 4+ CN
 rna_epi_cnv$gene_CN <- ifelse(test = rna_epi_cnv$gene_CN >= 4, 
                                        yes = "4+", no = rna_epi_cnv$gene_CN)
 
+#Plot the figure
 ggplot2::ggplot(data = rna_epi_cnv) +
   geom_boxplot(mapping = aes(x=STATE, y=RNAseq_Zscore,  fill = factor(gene_CN))) +
   theme_bw() +
