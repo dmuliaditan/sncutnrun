026f32309d7094b955376a455ec6f83ce22f73cc
Daniel Muliaditan <48524642+dmuliaditan@users.noreply.github.com>
2022-06-22 14:11:51 +0800

    Update 3_7_2_ChromHMM_promoter_subset_and_annotation.sh

--- 3. Genetic Driven Epigenetic Reprogramming Analysis/3_7_2_ChromHMM_promoter_subset_and_annotation.sh
+++ 3. Genetic Driven Epigenetic Reprogramming Analysis/3_7_2_ChromHMM_promoter_subset_and_annotation.sh
@@ -30,10 +30,6 @@ do
   -b /mnt/d/genome_references/hg38_reference/gencode.v32.basic.annotation.bed -g /mnt/d/genome_references/hg38_reference/resources_broad_hg38_v0_Homo_sapiens_assembly38.fasta.fai -d \
   > '/mnt/d/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_'"$j"'_'"$k"'_regions_nearest_gene.bed'
   
-  #Process to final bed
-  awk '{ a[$8]++ } END { for (b in a) { print b } }' '/mnt/d/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_'"$j"'_'"$k"'_regions_nearest_gene.bed' > \
-  '/mnt/d/snCUT_RUN/results/ChromHMM/HN137Pri_HN137Met_'"$j"'_'"$k"'_nearest_genes.bed'
-  
   done
  
 done
